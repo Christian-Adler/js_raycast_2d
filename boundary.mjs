ee66e1e3398f89e530e58e2f55f8dc8470c84f70
Christian Adler <chris.adler@gmx.de>
2024-06-30 05:40:49 +0200

    test colored walls

--- boundary.mjs
+++ boundary.mjs
@@ -1,8 +1,15 @@
+import {rad2deg} from "./utils.mjs";
+
+let idProvider = 0;
+
 class Boundary {
   constructor(vecA, vecB) {
+    this.id = idProvider++
     this.a = vecA;
     this.b = vecB;
     this.age = 0;
+
+    this.hslColorValRad = rad2deg(vecB.clone().subVec(vecA).toRadians());
   }
 
   hitByRay() {
@@ -12,7 +19,10 @@ class Boundary {
   draw(ctx) {
     if (this.age <= 0) return;
     this.age -= 0.001;
+
+    // ctx.strokeStyle = 'hsl(' + this.hslColorValRad + ', 100%, 50%, ' + (this.age) + ')';
     ctx.strokeStyle = "rgba(255,255,255," + this.age + ")";
+    // ctx.lineWidth = this.age * 3;
     ctx.beginPath();
     ctx.moveTo(this.a.x, this.a.y);
     ctx.lineTo(this.b.x, this.b.y);
